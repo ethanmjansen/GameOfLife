9da042bcff8ee7e7235281d2e139d49e7eed0eef
ethanmjansen <ethanmjansen@users.noreply.github.com>
2020-05-27 15:22:13 -0600

    Almost MVP

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from colors import COLORS
+from random import choice, randint
 
 # Root
 root = Tk()
@@ -24,42 +25,205 @@ frame3.pack(side = LEFT, fill=BOTH, padx=10)
 Generation = Label(frame3, text = f"Generation # {eight}", font=('Georgia', 16))
 Generation.pack()
 
-canvas = Canvas(frame3)
-canvas.pack(pady = 10)
-pilImage = Image.open("Bove Island.jpg")
-image = ImageTk.PhotoImage(pilImage)
-imagesprite = canvas.create_image(400,400,image=image)
+'''+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'''
 
+canvas = Canvas(frame3)
+canvas.pack()
+
+speed = 0
+
+class Cell:
+    def __init__(self, x, y, i, j):
+        self.isAlive = False
+        self.nextStatus = None
+        self.pos_screen = (x, y)
+        self.pos_matrix = (i, j)
+
+    def __str__(self):
+        return str(self.isAlive)
+
+    def __repr__(self):
+        return str(self.isAlive)
+
+    def switchStatus(self):
+        self.isAlive = not self.isAlive
+
+def create_grid():
+    """This function creates the board on which the game will take place"""
+    x = 10
+    y = 10
+    global grid # Variable to store the Cell objects
+    global rectangles # Variable to store rectangles
+    rectangles = []
+    grid = []
+    for i in range(25):
+        grid.append([])
+        rectangles.append([])
+        for j in range(35):
+            rect = canvas.create_rectangle(x, y, x+10, y+10, fill="white")
+            rectangles[i].append(rect)
+            grid[i].append(Cell(x, y, i, j))
+            x += 10
+        x = 10
+        y += 10
+
+
+def find_rect_coordinates(x, y):
+    """Find the co-ordinates of the rectangle which has been clicked"""
+    return (x- x%10, y - y%10)
+
+
+def change_colour_on_click(event):
+    """ Change the colour of the clicked grid and change the status of cell in the grid """
+    # print(event.x, event.y)
+    x, y = find_rect_coordinates(event.x, event.y)
+    try:
+        iy = x // 10 - 1
+        ix = y // 10 - 1
+        if ix == -1 or iy == -1:
+            raise IndexError
+        if grid[ix][iy].isAlive:
+            canvas.itemconfig(rectangles[ix][iy], fill="white")
+        else:
+            canvas.itemconfig(rectangles[ix][iy], fill="black")
+        grid[ix][iy].switchStatus()
+        # print(grid[ix][iy].pos_matrix, grid[ix][iy].pos_screen)
+    except IndexError:
+        return
+
+
+def paint_grid():
+    for i in grid:
+        for j in i:
+            if j.nextStatus != j.isAlive:
+                x, y = j.pos_matrix
+                # print(x, y)
+                if j.nextStatus:
+                    canvas.itemconfig(rectangles[x][y], fill="black")
+                    # canvas.itemconfig(rectangles[x][y], fill=choice(COLORS))
+                    # print("changed", j.pos_matrix, "from dead to alive")
+                else:
+                    canvas.itemconfig(rectangles[x][y], fill="white")
+                    # print("changed", j.pos_matrix, "from alive to dead")
+                j.switchStatus()
+                # print("Current status of", j.pos_matrix, j.isAlive)
+
+
+def changeInStatus(cell):
+    ''' If the cell's status changes in the next gen, return True else False '''
+    num_alive = 0
+    x, y = cell.pos_matrix
+    for i in (x-1, x, x+1):
+        for j in (y-1, y, y+1):
+            if i == x and j == y:
+                continue
+            if i == -1 or j == -1:
+                continue
+            try:
+                if grid[i][j].isAlive:
+                    num_alive += 1
+            except IndexError:
+                pass
+    if cell.isAlive:
+        return not( num_alive == 2 or num_alive == 3 )
+    else:
+        return num_alive == 3
+
+
+def begin_game():
+    global speed
+    for i in grid:
+        for j in i:
+            if changeInStatus(j):
+                j.nextStatus = not j.isAlive
+                # print("change in", j.pos_matrix, "from", j.isAlive, "to", j.nextStatus)
+            else:
+                j.nextStatus = j.isAlive
+    paint_grid()
+    global begin_id
+    begin_id = root.after(200, begin_game)
+
+
+def stop_game():
+    root.after_cancel(begin_id)    
+
+create_grid()
+canvas.bind("<Button-1>", change_colour_on_click)
+
+'''+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'''
 # Frame3 subframe
 # Frame 6
 frame6 = Frame(frame3)
 frame6.pack()
 
 # Controls
-Play = Button(frame6, text = "Play", font = ('Georgia', 12))
-Pause = Button(frame6, text = "Pause", font = ('Georgia', 12))
-Reset = Button(frame6, text = "Reset", font = ('Georgia', 12))
+Play = Button(frame6, text = "Play", font = ('Georgia', 12), command=begin_game)
+Pause = Button(frame6, text = "Pause", font = ('Georgia', 12), command=stop_game)
+Reset = Button(frame6, text = "Reset", font = ('Georgia', 12),command=create_grid)
 Play.pack(side = LEFT, padx=10)
 Pause.pack(side = LEFT, padx=10)
 Reset.pack(side = RIGHT, padx=10)
 
 # Frame 4 
 frame4 = Frame(frame1)
-frame4.pack(side = LEFT, fill=BOTH, padx = 10, pady = 10)
+frame4.pack(side = LEFT, fill=BOTH, padx = 10, pady = 40)
 
 # Frame 5
 frame5 = Frame(frame1)
 frame5.pack(side = LEFT,fill=BOTH,anchor=CENTER)
 
 # Presets
-Preset1 = Button(frame4, text = "Glider", font = ('Georgia', 12))
-Preset2 = Button(frame4, text = "Random", font = ('Georgia', 12))
-Preset3 = Button(frame4, text = "Space Ship", font = ('Georgia', 12))
-Preset4 = Button(frame4, text = "Glider Gun", font = ('Georgia', 12))
+def glider():
+    canvas.itemconfig(rectangles[21][4], fill="black")
+    grid[21][4].switchStatus()
+    canvas.itemconfig(rectangles[21][5], fill="black")
+    grid[21][5].switchStatus()
+    canvas.itemconfig(rectangles[21][6], fill="black")
+    grid[21][6].switchStatus()
+    canvas.itemconfig(rectangles[22][6], fill="black")
+    grid[22][6].switchStatus()
+    canvas.itemconfig(rectangles[23][5], fill="black")
+    grid[23][5].switchStatus()
+Preset1 = Button(frame4, text = "Glider", font = ('Georgia', 12), command=glider)
 Preset1.pack(anchor = W, pady=10)
+
+def random_config():
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            n = randint(0,3)
+            if n == 0:
+                canvas.itemconfig(rectangles[i][j], fill="black")
+                grid[i][j].switchStatus()
+Preset2 = Button(frame4, text = "Random", font = ('Georgia', 12), command=random_config)
 Preset2.pack(anchor = W, pady=10)
+
+def spaceship():
+    canvas.itemconfig(rectangles[12][32], fill="black")
+    grid[12][32].switchStatus()
+    canvas.itemconfig(rectangles[14][32], fill="black")
+    grid[14][32].switchStatus()
+    canvas.itemconfig(rectangles[15][31], fill="black")
+    grid[15][31].switchStatus()
+    canvas.itemconfig(rectangles[15][30], fill="black")
+    grid[15][30].switchStatus()
+    canvas.itemconfig(rectangles[15][29], fill="black")
+    grid[15][29].switchStatus()
+    canvas.itemconfig(rectangles[15][28], fill="black")
+    grid[15][28].switchStatus()
+    canvas.itemconfig(rectangles[15][27], fill="black")
+    grid[15][27].switchStatus()
+    canvas.itemconfig(rectangles[14][27], fill="black")
+    grid[14][27].switchStatus()
+    canvas.itemconfig(rectangles[13][27], fill="black")
+    grid[13][27].switchStatus()
+    canvas.itemconfig(rectangles[12][28], fill="black")
+    grid[12][28].switchStatus()
+    canvas.itemconfig(rectangles[11][30], fill="black")
+    grid[11][30].switchStatus()
+Preset3 = Button(frame4, text = "Space Ship", font = ('Georgia', 12), command=spaceship)
 Preset3.pack(anchor = W, pady=10)
-Preset4.pack(anchor = W, pady=10)
+
+
 
 # Rules
 QuoteBody = """
